f78a53a73a8d2e772bcadfad1e268d4fb02c73de
CS2071 <cs2071@kent.ac.uk>
2026-01-19 14:31:50 +0000

    floats don't work for now, fix later

--- partThree.py
+++ partThree.py
@@ -1,14 +1,16 @@
 def main():
     pounds = pounds_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to charge? "))
+    print(pounds, percent)
     charge = pounds * percent
     print(f"Charge £{charge:.2f}")
 
-
 def pounds_to_float(d):
-    # TODO
+    d = float(d)
+    # Neither of these def's return a value
+    # why
 
 def percent_to_float(p):
-    # TODO
+    p = float(p)*0.01
 
 main()
